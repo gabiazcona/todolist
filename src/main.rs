e35cad20554cfa7a02d5b195582c2f649b28832f
Gabi <azconita@gmail.com>
2022-05-07 21:41:13 +0200

    delete item by number

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::env;
 use std::error::Error;
-use std::fmt;
 use std::fs;
 use std::io::{self, BufRead};
 use std::io::prelude::*;
@@ -9,6 +8,7 @@ use std::str::FromStr;
 
 
 const FILENAME : &str = "todolist";
+const TMPFILE : &str = ".todolist.tmp";
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     if args.len() >= 2 {
@@ -20,6 +20,11 @@ fn main() -> Result<(), Box<dyn Error>> {
                     add_item(&args[2].clone())?;
                 }
             },
+            Command::Delete => {
+                if args.len() >= 3 {
+                    delete_item(&args[2].clone())?;
+                }
+            } 
             _ => invalid_command(),
         }
     } else {
@@ -75,8 +80,7 @@ fn add_item(item: &str) -> Result<(), Box<dyn Error>> {
         None => "1".to_string(),
         Some(Ok(line)) => {
             let number = line.split(" ").next().unwrap();
-            //let c = line.chars().next().unwrap();
-            let n: u32 = FromStr::from_str(number).unwrap();
+            let n: u32 = FromStr::from_str(number)?;
             (n+1).to_string()
             },
         Some(Err(_)) => panic!("error with file"),
@@ -87,17 +91,50 @@ fn add_item(item: &str) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-
-// type Result<T> = std::result::Result<T, FileError>;
-#[derive(Debug, Clone)]
-struct FileError;
-impl Error for FileError {}
-
-impl fmt::Display for FileError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "error operating on file")
+fn delete_item(item: &str) -> Result<(), Box<dyn Error>> {
+    // open todolist
+    let file = fs::OpenOptions::new()
+            .read(true)
+            .append(true)
+            .create(true)
+            .open(FILENAME)?;
+    // get line number
+    let line_to_delete: usize = FromStr::from_str(item).unwrap();
+    // get lines from file
+    let lines = io::BufReader::new(&file).lines();
+    // open new tmp file
+    let mut tmp_file = fs::File::create(TMPFILE)?;
+    // iterate over lines
+    let mut n = 1;
+    for line in lines {
+        let line = line?;
+        // if this is not the line to be deleted, don't write it 
+        if line_to_delete > n {
+            writeln!(&mut tmp_file, "{}", line)?;
+        } else if line_to_delete < n {
+            let mut line = line.split_inclusive(" ");
+            let number = line.next().unwrap();
+            let mut number: u32 = FromStr::from_str(&number[0..number.len()-1])?;
+            number -= 1;
+            let line: String = line.collect();
+            writeln!(&mut tmp_file, "{} {}", number.to_string(), line)?;
+        }
+        n += 1;
     }
+
+
+    // let lines: Vec<Result<String, std::io::Error>> = lines.collect();
+    // let mut lines: Vec<String> = lines.iter().map(|l| l.as_ref().unwrap().to_owned()).collect();
+    // lines.remove(line_to_delete-1);
+    
+    // // Write the other lines
+    // file.set_len(0)?;
+    // for line in lines {
+    //     writeln!(&mut file, "{}", line)?;
+    // }
+
+    fs::rename(TMPFILE, FILENAME)?;
+    Ok(())
 }
 
-// borrar item
 // marcar item como hecho
